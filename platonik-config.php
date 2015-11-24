2a8b89befd31885ef9f8aca79dd300e3b55d4793
Robert Korobkin <robertkorobkin@Robert-Korobkins-MacBook-Pro-3.local>
2015-11-24 12:13:38 -0500

    supporting multi-environment

--- platonik-config.php
+++ platonik-config.php
@@ -1,14 +1,26 @@
 <?php
 
-	$platonik_config = array(
+	$config = array();
+	if(strpos(getCwd(), "machigonne") !== false) $environment = "dev";
+	else $environment = "local";
 	
-		"database" => array(
-			"servername" => "localhost", 
-			"username" => "root", 
-			"password" => "root",
-			"database" => "platonik"
-		)
+	switch($environment) {
+		case "dev" : 
+			$config["database"] = array(
+				"servername" => "localhost"
+			);
+			$config["appId"] = '1079145595453451';
+		break;
+
+		case "local" : 
+			$config["database"] = array(
+				"servername" => "localhost", 
+				"username" => "root", 
+				"password" => "root",
+				"database" => "platonik"
+			);
+			$config["appId"] = '1079145595453451';
+		break;
+	}
 	
-	
-	);
-	
+	$platonik_config = $config;
